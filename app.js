542dfdd2409230e422cf0a1645b986eb757d9916
Siddharth-DWT <smishra9120@gmail.com>
2023-01-23 17:59:40 +0530

    Stripe Checkout and Webhook Endpoint Added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const cors = require("cors");
 const { Client } = require("pg");
 const dintDistributerABI = require("./DintDistributerABI.json");
 const fernet = require("fernet");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const buffer = require("micro");
+
+
 
 const app = express();
 const PORT = 5000;
@@ -60,6 +64,7 @@ const DintTokenAddress = process.env.DINT_TOKEN_ADDRESS;
 const DintDistributerAddress = process.env.DINT_DIST_ADDRESS;
 const ownerPrivateKey = process.env.OWNER_PRIVATE_KEY;
 const web3 = new Web3(process.env.RPC_PROVIDER);
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
 
@@ -321,6 +326,35 @@ const getData = async (sender_id, reciever_id, amount) => {
   });
 };
 
+const transferDint = async ({ amount, destAddr }) => {
+
+  const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
+  const abi = [
+    {
+      constant: false,
+      inputs: [
+        { name: "_to", type: "address" },
+        { name: "_amount", type: "uint256" },
+      ],
+      name: "transfer",
+      outputs: [{ name: "success", type: "bool" }],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+
+  const contractAddr = process.env.DINT_TOKEN_ADDRESS;
+  const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+  const gasPrice = await provider.getGasPrice();
+  const tx = await erc20dint.transfer(destAddr, amount, {
+    gasPrice,
+  }); // TRANSFER DINT to the customer
+
+  return tx;
+};
+
+
 app.post("/api/send-dint/", async (req, res) => {
   // console.log("req.headers", req.headers.apikey=== process.env.SECURITY_KEY);
   if (req.headers.apikey !== process.env.SECURITY_KEY) {
@@ -368,6 +402,63 @@ app.post("/api/send-dint/", async (req, res) => {
     res.status(500).json({ sucess: false, message: "Something went wrong. Please try again!" });
   }
 });
+
+ app.post("/api/checkout/", async (req, res) => {
+   const { walletAddr, amount, email } = req.body;
+   const session = await stripe.checkout.sessions.create({
+     payment_method_types: ["card"],
+     customer_email: email,
+     // pass customer wallet addr as metadata, so we know where to transfer funds
+     payment_intent_data: {
+       metadata: {
+         walletAddr: walletAddr,
+       },
+     },
+     metadata: {
+       walletAddr: walletAddr,
+     },
+     line_items: [
+       {
+         price_data: {
+           currency: "usd",
+           product_data: {
+             name: "Membership credits", // name of the product (shown at checkout)
+           },
+           unit_amount: Number(amount) * 100, // Stripe accepts prices in cents
+         },
+         quantity: 1,
+       },
+     ],
+     mode: "payment",
+     success_url: `https://fedev.dint.com/buy-dint-token`, // where redirect user after success/fail
+     cancel_url: `https://fedev.dint.com/buy-dint-token`,
+   });
+   res.status(200).json({ session });
+ });
+ 
+//For Stripe Webhook
+ app.post("/api/webhooks/stripe/", async (req, res) => { res.setHeader("Access-Control-Allow-Origin", "*");
+
+ try {
+   const buf = await buffer(req);
+   const sig = req.headers["stripe-signature"];
+   // make sure that webhook is called by Stripe (not hackers or other ppl)
+   // make sure that Stripe is configured to emit events to your webhook!
+   const event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+   if (event.type === "payment_intent.succeeded") {
+     const amount = ethers.utils.parseEther(
+       String(event.data.object.amount / 100)
+     );
+     const destAddr = event.data.object.metadata.walletAddr;
+     console.log({ amount, destAddr });
+     const tx = await transferDint({ amount, destAddr });
+     console.log("tx hash", tx.hash);
+   }
+   res.status(200).json({ received: true });
+ } catch (err) {
+   res.status(400).send(`Webhook Error: ${err.message}`);
+ }})
+
 
 app.listen(PORT, (error) => {
   if (!error)
