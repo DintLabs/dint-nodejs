602d18b9cb9b4418b40955256e9b433f2a5e345f
Siddharth-DWT <smishra9120@gmail.com>
2023-01-24 20:19:44 +0530

    log added

--- controller/dint.js
+++ controller/dint.js
@@ -26,11 +26,7 @@ const ownerPrivateKey = process.env.OWNER_PRIVATE_KEY;
 const web3 = new Web3(process.env.RPC_PROVIDER);
 // const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-
-
-const provider = new ethers.providers.JsonRpcProvider(
-  process.env.RPC_PROVIDER 
-);
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
 
 const ownerSigner = new ethers.Wallet(ownerPrivateKey, provider);
 
--- routes/payment.js
+++ routes/payment.js
@@ -1,37 +1,119 @@
+const winston = require("winston");
+const expressWinston = require("express-winston");
 const express = require("express");
 const stripe = require("stripe");
 const ethers = require("ethers");
+// require("dotenv").config({ path: `../env.local`, override: true });
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const { transferDint } = require("../controller/stripe");
 const stripeApp = express.Router();
 stripeApp.use(express.raw({ type: "*/*" }));
-// require("dotenv").config({ path: `../env.local`, override: true });
+stripeApp.use(
+  bodyParser.json({
+    verify: function (req, res, buf) {
+      var url = req.originalUrl;
+      if (url.startsWith("/stripe")) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
+require("dotenv").config({ path: `../env.local`, override: true });
 require("dotenv").config();
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+logger.log({
+  level: "info",
+  message: "What time is the testing at?",
+});
 
+// stripeApp.use(
+//   expressWinston.logger({
+//     transports: [
+//       new winston.transports.Console(),
+//     //   new winston.transports.File({ filename: "combined.log" }),
+//     ],
+//     format: winston.format.combine(
+//       winston.format.colorize(),
+//       winston.format.json()
+//     ),
+//     meta: false,
+//     msg: "HTTP  ",
+//     expressFormat: true,
+//     colorize: false,
+//     ignoreRoute: function (req, res) {
+//       return false;
+//     },
+//   })
+// );
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-stripeApp.post("/stripe", async (req, res) => {
+stripeApp.post("/stripe/", async (req, res) => {
   // This is your Stripe CLI webhook secret for testing your endpoint locally.
+  logger.log({
+    level: "info",
+    message: "End Point Hit",
+  });
 
   const sig = req.headers["stripe-signature"];
+  console.log("error", "error");
+  winston.log("error", "127.0.0.1 - there's no place like home");
 
   let event;
 
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    logger.log({
+      level: "info",
+      message: "Event Created",
+      event,
+    });
+    logger.log("info", event);
+    // logger.info(event);
+
+    console.log("event", event);
     if (event.type === "payment_intent.succeeded") {
+      logger.log({
+        level: "error",
+        message: "payment_intent.succeeded found",
+      });
+
       const amount = ethers.utils.parseEther(
         String(event.data.object.amount / 100)
       );
       const destAddr = event.data.object.metadata.walletAddr;
-      console.log({ amount, destAddr });
+      console.log(amount, destAddr);
       const tx = await transferDint({ amount, destAddr });
+
       console.log("tx hash", tx.hash);
     } else {
       res.status(200).json({ received: false });
+      logger.log({
+        level: "error",
+        message: event.type,
+      });
+      // myLibLog.info("Payment not succeeded");
     }
   } catch (err) {
+    logger.log({
+      level: "error",
+      message: "Event Failed",
+    });
     res.status(400).send(`Webhook Error: ${err.message}`);
+
     return;
   }
 
