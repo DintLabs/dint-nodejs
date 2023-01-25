65327763908ae5975675b51238768e627d7a932a
Max <info@dint.com>
2023-01-25 07:55:39 -0500

    redirect to https://fedev.dint.com/dint-wallet

--- controller/dint.js
+++ controller/dint.js
@@ -307,8 +307,8 @@ const checkout = async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: `https://fedev.dint.com/buy-dint-token`, // where redirect user after success/fail
-    cancel_url: `https://fedev.dint.com/buy-dint-token`,
+    success_url: `https://fedev.dint.com/dint-wallet`, // where redirect user after success/fail
+    cancel_url: `https://fedev.dint.com/dint-wallet`,
   });
   res.status(200).json({ session });
 };
