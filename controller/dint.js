a8532864a4a463ed1c37cb11bc2985ef0916492a
Max <info@dint.com>
2023-02-10 21:36:58 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -280,57 +280,39 @@ const getData = async (sender_id, reciever_id, amount) => {
   });
 };
 
-
 const checkout = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  
-  const { walletAddr, email, amount, cardDetails } = req.body;
+   const { walletAddr, amount, email } = req.body;
+   const session = await stripe.checkout.sessions.create({
+     payment_method_types: ["card"],
+     customer_email: email,
+     // pass customer wallet addr as metadata, so we know where to transfer funds
+     payment_intent_data: {
+       metadata: {
+         walletAddr: walletAddr,
+       },
+     },
+     metadata: {
+       walletAddr: walletAddr,
+     },
+     line_items: [
+       {
+         price_data: {
+           currency: "usd",
+           product_data: {
+             name: "Membership credits", // name of the product (shown at checkout)
+           },
+           unit_amount: Number(amount) * 100, // Stripe accepts prices in cents
+         },
+         quantity: 1,
+       },
+     ],
+     mode: "payment",
+     success_url: `https://dint.com/dint-wallet`, // where redirect user after success/fail
+     cancel_url: `https://dint.com/dint-wallet`,
 
-  // Make sure a customer ID is provided
-  if (!cardDetails || !cardDetails.customer_id) {
-    return res.status(400).send({ error: "A customer ID must be provided." });
-  }
-
-  // Create the charge
-  const charge = await stripe.charges.create({
-    receipt_email: req.body.email,
-    amount: parseInt(req.body.amount) * 100, // convert amount to cents
-    currency: "usd",
-    card: req.body.cardDetails.card_id,
-    customer: req.body.cardDetails.customer_id,
-    metadata: {
-      walletAddr: walletAddr,
-    },
-  });
-
-  // Handle payment_intent.succeeded event
-  if (charge.status === "succeeded") {
-    console.log("Payment was successful.");
-    const payment_intent_data = {
-      metadata: {
-        walletAddr: walletAddr
-      }
-    };
-  } else {
-    console.error("Payment failed.");
-  }
-  const event = await stripe.events.create({
-    type: "payment_intent.succeeded",
-    data: {
-      object: {
-        id: charge.payment_intent,
-        amount: charge.amount,
-        currency: charge.currency,
-        object: "payment_intent",
-        status: "succeeded",
-        metadata: {
-          walletAddr: walletAddr,
-        },
-      },
-    },
-  });
-
-  res.send({ charge, walletAddr, email });
-};
-
-module.exports = { checkout };
+    });
+    res.status(200).json({ session });
+  };
+ 
+  module.exports = { getData, generate, checkout };
