5add60e8fce2af706fca1291506442e54d8b9b4a
Max <info@dint.com>
2023-02-10 19:23:52 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -281,37 +281,36 @@ const getData = async (sender_id, reciever_id, amount) => {
 };
 
 const checkout = async (req, res) => {
- res.setHeader("Access-Control-Allow-Origin", "*");
-  const { walletAddr, amount, email } = req.body;
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    customer_email: req.body.cardDetails.customer_id,
-    // pass customer wallet addr as metadata, so we know where to transfer funds
-    payment_intent_data: {
-      metadata: {
-        walletAddr: walletAddr,
-      },
-    },
-    metadata: {
-      walletAddr: walletAddr,
-    },
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "Membership credits", // name of the product (shown at checkout)
-          },
-          unit_amount: Number(amount) * 100, // Stripe accepts prices in cents
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `https://dint.com/dint-wallet`, // where redirect user after success/fail
-    cancel_url: `https://dint.com/dint-wallet`,
-  });
-  res.status(200).json({ session });
-};
-
-module.exports = { getData, generate, checkout };
+  res.setHeader("Access-Control-Allow-Origin", "*");
+   const { walletAddr, amount, email } = req.body;
+   const session = await stripe.checkout.sessions.create({
+     payment_method_types: ["card"],
+     customer: req.body.cardDetails.customer_id,
+     payment_intent_data: {
+       metadata: {
+         walletAddr: walletAddr,
+       },
+     },
+     metadata: {
+       walletAddr: walletAddr,
+     },
+     line_items: [
+       {
+         price_data: {
+           currency: "usd",
+           product_data: {
+             name: "Membership credits", // name of the product (shown at checkout)
+           },
+           unit_amount: Number(amount) * 100, // Stripe accepts prices in cents
+         },
+         quantity: 1,
+       },
+     ],
+     mode: "payment",
+     success_url: `https://dint.com/dint-wallet`, // where redirect user after success/fail
+     cancel_url: `https://dint.com/dint-wallet`,
+   });
+   res.status(200).json({ session });
+ };
+ 
+ module.exports = { getData, generate, checkout };
