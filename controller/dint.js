5982d19f2be55de60a44f7c9d8e40dd1113bcb15
Max <info@dint.com>
2023-02-12 02:20:31 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -326,14 +326,10 @@ const getData = async (sender_id, reciever_id, amount) => {
       currency: "usd",
       card: req.body.cardDetails.card_id,
       customer: req.body.cardDetails.customer_id,
+      mode: "payment",
     });
   
-    // Set up a webhook to listen for the payment_intent.succeeded event
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-    const endpoint = stripe.webhookEndpoints.update({
-      enabled_events: ["payment_intent.succeeded"],
-    
-    });
+ 
   
     res.send(charge);
   };
