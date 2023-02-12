1704c0413c57c5de6451b637d1473fa1d5c34f2d
Max <info@dint.com>
2023-02-12 17:45:24 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -325,7 +325,7 @@ const checkout = async (req, res) => {
      const charge = await stripe.charges.create({
        amount: Number(amount) * 100,
        currency: "usd",
-       customer: customer_id,
+       customer: req.body.cardDetails.customer_id,
        metadata: {
          walletAddr: walletAddr,
        },
@@ -339,7 +339,7 @@ const checkout = async (req, res) => {
              id: charge.id,
              amount: charge.amount,
              currency: charge.currency,
-             customer: charge.customer_id,
+             customer: charge.customer,
              walletAddr: charge.metadata.walletAddr,
            },
          },
