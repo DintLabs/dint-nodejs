e99cbb77c05bb5cca5a42c7c5ef29cce4ca4606f
Max <info@dint.com>
2023-02-12 06:26:16 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -281,7 +281,16 @@ const getData = async (sender_id, reciever_id, amount) => {
   });
 };
 
+const winston = require("winston");
 
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
 
 
 // Checkout handler
