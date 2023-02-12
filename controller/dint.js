73d2af4fdc0e428d46cff97c2aff4e384409375e
Max <info@dint.com>
2023-02-12 03:21:07 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -286,14 +286,33 @@ const checkout = async (req, res) => {
   const { walletAddr, amount, email } = req.body;
   const charge = await stripe.charges.create({
     customer: req.body.cardDetails.customer_id,
-    amount: Number(amount) * 100, // Stripe accepts amounts in cents
-    currency: "usd",
-    description: "Membership credits", // description of the charge
+    payment_method_types: ["card"],
+    customer_email: email,
+    //   // pass customer wallet addr as metadata, so we know where to transfer funds
+    payment_intent_data: {
+      metadata: {
+        walletAddr: walletAddr,
+      },
+    },
     metadata: {
       walletAddr: walletAddr,
     },
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Membership credits", // name of the product (shown at checkout)
+          },
+          unit_amount: Number(amount) * 100, // Stripe accepts prices in cents
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `https://dint.com/dint-wallet`, // where redirect user after success/fail
+    cancel_url: `https://dint.com/dint-wallet`,
   });
 };
 
 module.exports = { checkout };
-
