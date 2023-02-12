175b2803c5380d2dc4b3e148939b00995cca9356
Max <info@dint.com>
2023-02-12 05:38:23 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -7,6 +7,7 @@ const { Client } = require("pg");
 const dintDistributerABI = require("../DintDistributerABI.json");
 const fernet = require("fernet");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const express = require('express');
 const app = express();
 const client = new Client({
   user: process.env.DB_USER,
