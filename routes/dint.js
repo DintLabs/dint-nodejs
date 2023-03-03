208ffd32ccb8d2a01234cd10f64584dca7527456
Max <info@dint.com>
2023-03-03 16:14:29 -0500

    Update dint.js

--- routes/dint.js
+++ routes/dint.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const sendDint = express.Router();
+// require("dotenv").config({ path: `../env.local`, override: true });
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const { ethers } = require("ethers");
 const { getData, generate, checkout } = require("../controller/dint");
 const { approval, getUserData } = require("../controller/payout");
 
@@ -15,52 +15,44 @@ sendDint.use(bodyParser.json());
 
 sendDint.post("/send-dint", async (req, res) => {
   if (req.headers.apikey !== process.env.SECURITY_KEY) {
+    console.log("req.headers", req.headers.apikey === process.env.SECURITY_KEY);
+
     return res.send({ success: false, message: "invalid api key" });
   }
-
   if (!process.env.OWNER_PRIVATE_KEY) {
     return res.send({ success: false, message: "private key not found" });
   }
 
-  const { sender_id, reciever_id, amount, priceInUSD } = req.body;
+  const { sender_id, reciever_id, amount } = req.body;
 
   try {
     getData(sender_id, reciever_id, amount)
       .then((data) => {
         generate(data, amount)
-          .then(async (data) => {
-            const provider = new ethers.providers.JsonRpcProvider();
-            const wallet = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
-
-            const tx = {
-              to: data.data.recieverAddress,
-              value: ethers.utils.parseEther(amount),
-              gasPrice: await provider.getGasPrice(),
-            };
-
-            wallet.sendTransaction(tx).then((transaction) => {
-              return res.send({
-                success: true,
-                hash: transaction.hash,
-                sender: data.data.userAddress,
-                reciever: data.data.recieverAddress,
-                amount: amount,
-                priceInUSD: priceInUSD,
-              });
-            }).catch((err) => {
-              return res.send({
-                success: false,
-                message:
-                  "Something went wrong while making transaction. Please try again!",
-                error: err,
-              });
+          .then((data) => {
+            // if (data.data) {
+            //   const users = ethers.utils.defaultAbiCoder.decode(
+            //     ["address", "address"],
+            //     data.data
+            //   );
+            //   const sender = users[0];
+            //   const reciever = users[1];
+            return res.send({
+              success: true,
+              Hash: data.res.hash,
+              sender: data.data.userAddress,
+              reciever: data.data.recieverAddress,
+              amount: amount,
             });
+            // } else {
+            //   return res.send("Something went wrong. Please try again");
+            // }
           })
           .catch((err) => {
             return res.send({
               success: false,
               message:
-                "Something went wrong while generating the transaction. Please try again!",
+                "Something went wrong while making transaction. Please try again!",
               error: err,
             });
           });
@@ -83,16 +75,19 @@ sendDint.post("/send-dint", async (req, res) => {
 sendDint.post("/checkout", checkout);
 
 sendDint.post("/withdraw-dint", async (req, res) => {
-  if (req.headers.apikey !== process.env.SECURITY_KEY) {
-    return res.send({ success: false, message: "invalid api key" });
-  }
+   if (req.headers.apikey !== process.env.SECURITY_KEY) {
+     console.log(
+       "req.headers",
+       req.headers.apikey === process.env.SECURITY_KEY
+     );
 
-  if (!process.env.OWNER_PRIVATE_KEY) {
-    return res.send({ success: false, message: "private key not found" });
-  }
-
+     return res.send({ success: false, message: "invalid api key" });
+   }
+   if (!process.env.OWNER_PRIVATE_KEY) {
+     return res.send({ success: false, message: "private key not found" });
+   }
   const { user_id, amount } = req.body;
-
+  console.log(" req.body", req.body);
 
   try {
     getUserData(user_id, amount)
