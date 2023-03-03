7b199e22f38b174f5b1987ab7331c772ab23993d
Max <info@dint.com>
2023-03-03 17:04:20 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -198,7 +198,7 @@ const send = async (data, value) => {
   );
   return new Promise((resolve, reject) => {
     dintDistContract
-      .sendDint(data.userAddress, data.recieverAddress, value, {
+      .sendDint(data.userAddress, data.recieverAddress, value, _priceInUSD,{
         gasLimit: 1000000,
         gasPrice: 30000000000,
       })
@@ -237,7 +237,7 @@ const send = async (data, value) => {
   });
 };
 
-const getData = async (sender_id, reciever_id, amount, _priceInUSD) => {
+const getData = async (sender_id, reciever_id, amount) => {
   return new Promise((resolve, reject) => {
     client
       .query(
