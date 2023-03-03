ef21de85a5030ae9e1625ecc314ee52f8f7d8b2a
Max <info@dint.com>
2023-03-03 14:55:36 -0500

    Update dint.js

--- routes/dint.js
+++ routes/dint.js
@@ -24,11 +24,12 @@ sendDint.post("/send-dint", async (req, res) => {
   }
 
   const { sender_id, reciever_id, amount } = req.body;
+  const priceInUSD = 1000000;
 
   try {
     getData(sender_id, reciever_id, amount)
       .then((data) => {
-        generate(data, amount)
+        generate(data, amount, priceInUSD)
           .then((data) => {
             // if (data.data) {
             //   const users = ethers.utils.defaultAbiCoder.decode(
