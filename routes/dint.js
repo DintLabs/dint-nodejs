2d14ccfd1b67238b38630dbedada50b7fb9d4651
Max <info@dint.com>
2023-03-03 16:04:18 -0500

    Update dint.js

--- routes/dint.js
+++ routes/dint.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sendDint = express.Router();
+// require("dotenv").config({ path: `../env.local`, override: true });
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const { getData, generate, checkout } = require("../controller/dint");
@@ -14,6 +15,8 @@ sendDint.use(bodyParser.json());
 
 sendDint.post("/send-dint", async (req, res) => {
   if (req.headers.apikey !== process.env.SECURITY_KEY) {
+    console.log("req.headers", req.headers.apikey === process.env.SECURITY_KEY);
+
     return res.send({ success: false, message: "invalid api key" });
   }
   if (!process.env.OWNER_PRIVATE_KEY) {
@@ -21,20 +24,29 @@ sendDint.post("/send-dint", async (req, res) => {
   }
 
   const { sender_id, reciever_id, amount } = req.body;
-  const priceInUSD = 10000;
+
   try {
     getData(sender_id, reciever_id, amount)
       .then((data) => {
-        generate(data, amount, priceInUSD)
+        generate(data, amount)
           .then((data) => {
+            // if (data.data) {
+            //   const users = ethers.utils.defaultAbiCoder.decode(
+            //     ["address", "address"],
+            //     data.data
+            //   );
+            //   const sender = users[0];
+            //   const reciever = users[1];
             return res.send({
               success: true,
               Hash: data.res.hash,
               sender: data.data.userAddress,
               reciever: data.data.recieverAddress,
               amount: amount,
-              priceInUSD: priceInUSD,
             });
+            // } else {
+            //   return res.send("Something went wrong. Please try again");
+            // }
           })
           .catch((err) => {
             return res.send({
@@ -46,6 +58,7 @@ sendDint.post("/send-dint", async (req, res) => {
           });
       })
       .catch((error) => {
+        console.log("err", error);
         return res.send({
           sucess: false,
           message: "Something went wrong while getting user data.",
@@ -62,25 +75,41 @@ sendDint.post("/send-dint", async (req, res) => {
 sendDint.post("/checkout", checkout);
 
 sendDint.post("/withdraw-dint", async (req, res) => {
-  if (req.headers.apikey !== process.env.SECURITY_KEY) {
-    return res.send({ success: false, message: "invalid api key" });
-  }
-  if (!process.env.OWNER_PRIVATE_KEY) {
-    return res.send({ success: false, message: "private key not found" });
-  }
+   if (req.headers.apikey !== process.env.SECURITY_KEY) {
+     console.log(
+       "req.headers",
+       req.headers.apikey === process.env.SECURITY_KEY
+     );
+
+     return res.send({ success: false, message: "invalid api key" });
+   }
+   if (!process.env.OWNER_PRIVATE_KEY) {
+     return res.send({ success: false, message: "private key not found" });
+   }
   const { user_id, amount } = req.body;
+  console.log(" req.body", req.body);
 
   try {
     getUserData(user_id, amount)
       .then((data) => {
         approval(data, amount)
           .then((data) => {
+            // if (data.data) {
+            //   const users = ethers.utils.defaultAbiCoder.decode(
+            //     ["address", "address"],
+            //     data.data
+            //   );
+            //   const sender = users[0];
+            //   const reciever = users[1];
             return res.send({
               success: true,
               hash: data.res.hash,
               userAddress: data.data.userAddress,
               amount: amount,
             });
+            // } else {
+            //   return res.send("Something went wrong. Please try again");
+            // }
           })
           .catch((err) => {
             return res.send({
@@ -92,6 +121,7 @@ sendDint.post("/withdraw-dint", async (req, res) => {
           });
       })
       .catch((error) => {
+        console.log("err", error);
         return res.send({
           sucess: false,
           message: "Something went wrong while getting user data.",
@@ -106,5 +136,7 @@ sendDint.post("/withdraw-dint", async (req, res) => {
     });
   }
 });
+
+
 
 module.exports = sendDint;
