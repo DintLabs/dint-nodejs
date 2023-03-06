67f4e9cd1ce5bffa5588e221cf1419842c2d4920
Max <info@dint.com>
2023-03-05 19:16:34 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -26,12 +26,21 @@ const transferDint = async ({ amount, destAddr }) => {
     },
   ];
 
+
+n
+
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
-  const gasPrice = await provider.getGasPrice();
+
+  const gasLimit = 21000; // Standard gas limit for a simple transfer
+  const targetBlocks = 3; // Target number of blocks for the transaction to be included in
+  const gasPrice = await provider.estimateGasPrice({ targetBlockTime: targetBlocks * 15 }); // Target block time is 15 seconds on Polygo
   const tx = await erc20dint.transfer(destAddr, amount, {
     gasPrice,
+    gasLimit,
   }); // TRANSFER DINT to the customer
+
+
 
   return tx;
 };
