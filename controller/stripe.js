b821432ae3b884bc3a1bd931d99a8c581ca29e69
Max <info@dint.com>
2023-03-06 13:54:22 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,5 +1,5 @@
 const ethers = require("ethers");
-const axios = require("axios");
+// require("dotenv").config({ path: `../env.local`, override: true });
 require("dotenv").config();
 
 const transferDint = async ({ amount, destAddr }) => {
@@ -8,10 +8,9 @@ const transferDint = async ({ amount, destAddr }) => {
   );
 
   const signer = new ethers.Wallet(
-    process.env.OWNER_PRIVATE_KEY,
+    (process.env.OWNER_PRIVATE_KEY),
     provider
   );
-
   const abi = [
     {
       constant: false,
@@ -27,35 +26,11 @@ const transferDint = async ({ amount, destAddr }) => {
     },
   ];
 
-  // Get the current gas prices
-  let gasPrice;
-  let maxFeePerGas;
-  let maxPriorityFeePerGas;
-  try {
-    const { data } = await axios.get(
-      "https://gasstation-mainnet.matic.network/v2"
-    );
-    const gasPrices = data.data;
-    gasPrice = gasPrices.fast;
-    maxFeePerGas = ethers.utils.parseUnits(gasPrice.toString(), "gwei");
-    maxPriorityFeePerGas = ethers.utils.parseUnits(
-      (gasPrice - 5).toString(),
-      "gwei"
-    );
-  } catch (error) {
-    console.error("Error fetching gas prices:", error);
-    maxFeePerGas = ethers.utils.parseUnits("200", "gwei"); // Set default gas price
-    maxPriorityFeePerGas = ethers.utils.parseUnits("35", "gwei"); // Set default priority gas price
-  }
-
-  // Send the transaction
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
-  const gasLimit = await erc20dint.estimateGas.transfer(destAddr, amount);
+  const gasPrice = ethers.utils.parseUnits("200", "gwei");
   const tx = await erc20dint.transfer(destAddr, amount, {
-    gasLimit,
-    maxFeePerGas,
-    maxPriorityFeePerGas,
+    gasPrice,
   }); // TRANSFER DINT to the customer
 
   return tx;
