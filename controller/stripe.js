e5fe1201772590c64c510d7d9f8af80d374cef9c
Max <info@dint.com>
2023-03-06 20:49:27 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,5 +1,5 @@
 const ethers = require("ethers");
-// require("dotenv").config({ path: `../env.local`, override: true });
+const axios = require("axios");
 require("dotenv").config();
 
 const transferDint = async ({ amount, destAddr }) => {
@@ -8,9 +8,10 @@ const transferDint = async ({ amount, destAddr }) => {
   );
 
   const signer = new ethers.Wallet(
-    (process.env.OWNER_PRIVATE_KEY),
+    process.env.OWNER_PRIVATE_KEY,
     provider
   );
+
   const abi = [
     {
       constant: false,
@@ -28,12 +29,39 @@ const transferDint = async ({ amount, destAddr }) => {
 
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
-  const gasPrice = await provider.getGasPrice();
-  const tx = await erc20dint.transfer(destAddr, amount, {
-    gasPrice,
-  }); // TRANSFER DINT to the customer
 
+  // Get the current gas prices
+  let gasPrice;
+  const gasLimit = await erc20dint.estimateGas.transfer(destAddr, amount);
+
+  let maxFeePerGas;
+  let maxPriorityFeePerGas;
+  try {
+    const { data } = await axios.get(
+      "https://gasstation-mainnet.matic.network/v2"
+    );
+    const gasPrices = data.data;
+    gasPrice = gasPrices.fast;
+    maxFeePerGas = ethers.utils.parseUnits(gasPrice.toString(), "gwei");
+    maxPriorityFeePerGas = ethers.utils.parseUnits(
+      (gasPrice - 5).toString(),
+      "gwei"
+    );
+  } catch (error) {
+    console.error("Error fetching gas prices:", error);
+    maxFeePerGas = ethers.utils.parseUnits("200", "gwei"); // Set default gas price
+    maxPriorityFeePerGas = ethers.utils.parseUnits("35", "gwei"); // Set default priority gas price
+  }
+
+  // Send the transaction
+  const tx = await erc20dint.transfer(destAddr, amount, {
+    gasLimit,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  console.log("Transaction Hash", tx.hash);
   return tx;
+
 };
 
 module.exports = { transferDint };
