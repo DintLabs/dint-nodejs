b722f02a71678ed7eb9aaf0926c952b6b6b67b40
Max <info@dint.com>
2023-03-07 15:15:31 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,11 +1,16 @@
-const { ethers } = require("ethers");
+const ethers = require("ethers");
 const axios = require("axios");
 require("dotenv").config();
 
 const transferDint = async ({ amount, destAddr }) => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
-  const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
-  const contractAddr = process.env.DINT_TOKEN_ADDRESS;
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_PROVIDER
+  );
+
+  const signer = new ethers.Wallet(
+    process.env.OWNER_PRIVATE_KEY,
+    provider
+  );
 
   const abi = [
     {
@@ -22,44 +27,48 @@ const transferDint = async ({ amount, destAddr }) => {
     },
   ];
 
+  const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
 
   // Get the current gas prices
   let gasPrice;
-  let maxFeePerGas = ethers.BigNumber.from(40000000000); // fallback to 40 gwei
-  let maxPriorityFeePerGas = ethers.BigNumber.from(40000000000); // fallback to 40 gwei
+  let maxFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
+  let maxPriorityFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
   try {
     const { data } = await axios({
-      method: "get",
-      url: "https://gasstation-mainnet.matic.network/v2",
+      method: 'get',
+      url: process.env.IS_PROD === 'true'
+      ? 'https://gasstation-mainnet.matic.network/v2'
+      : 'https://gasstation-mumbai.matic.today/v2',
     });
 
-    console.log("Gas prices:", data);
+    console.log('Gas prices:', data); // log the entire response object
     const gasPrices = data;
-    if (gasPrices && gasPrices.hasOwnProperty("fast")) {
-      maxFeePerGas = ethers.utils.parseUnits(gasPrices.fast.toString(), "gwei");
+    if (gasPrices && gasPrices.fast && typeof gasPrices.fast.maxFeePerGas === 'number') {
+      maxFeePerGas = ethers.utils.parseUnits(gasPrices.fast.maxFeePerGas.toString(), "gwei");
       maxPriorityFeePerGas = ethers.utils.parseUnits(
-        (gasPrices.fast - 5).toString(),
+        (gasPrices.fast.maxFeePerGas - 5).toString(),
         "gwei"
       );
-      gasPrice = gasPrices.fast;
+      gasPrice = gasPrices.fast.maxFeePerGas;
     } else {
       // handle the error or fallback to a default gas price
       gasPrice = 200;
-      maxFeePerGas = ethers.BigNumber.from(90000000000); // fallback to 40 gwei
-      maxPriorityFeePerGas = ethers.BigNumber.from(70000000000); // fallback to 40 gwei
+      maxFeePerGas = ethers.utils.parseUnits("40", "gwei"); // fallback to 40 gwei
+      maxPriorityFeePerGas = ethers.utils.parseUnits("40", "gwei"); // fallback to 40 gwei
     }
   } catch (error) {
     console.error("Error fetching gas prices:", error);
     gasPrice = 200;
-    maxFeePerGas = ethers.utils.parseUnits("200", "gwei"); // Set default gas price
-    maxPriorityFeePerGas = ethers.utils.parseUnits("200", "gwei"); // Set default priority gas price
+    maxFeePerGas = ethers.utils.parseUnits("40", "gwei"); // Set default gas price
+    maxPriorityFeePerGas = ethers.utils.parseUnits("40", "gwei"); // Set default priority gas price
   }
 
   // Estimate gas limit
   let gasLimit = await erc20dint.estimateGas.transfer(destAddr, amount);
   const GAS_MULTIPLIER = 2;
   gasLimit = parseInt(gasLimit * GAS_MULTIPLIER);
+
 
   // Send the transaction
   const tx = await erc20dint.transfer(destAddr, amount, {
