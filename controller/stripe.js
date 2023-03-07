1d207f38735360ae7e9902d5baa55220dbf50068
Max <info@dint.com>
2023-03-07 17:41:05 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,12 +1,16 @@
 const ethers = require("ethers");
-const axios = require("axios");
+// require("dotenv").config({ path: `../env.local`, override: true });
 require("dotenv").config();
 
 const transferDint = async ({ amount, destAddr }) => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_PROVIDER
+  );
 
-  const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
-
+  const signer = new ethers.Wallet(
+    (process.env.OWNER_PRIVATE_KEY),
+    provider
+  );
   const abi = [
     {
       constant: false,
@@ -24,45 +28,15 @@ const transferDint = async ({ amount, destAddr }) => {
 
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+  const gasPrice = await provider.getGasPrice();
+  
+  const tx = await erc20dint.transfer(destAddr, amount, {
+    gasPrice,
+  }); // TRANSFER DINT to the customer
 
-  // get max fees from gas station
-  let maxFeePerGas = ethers.BigNumber.from(150000000000); // fallback to 40 gwei
-  let maxPriorityFeePerGas = ethers.BigNumber.from(1500000000000); // fallback to 40 gwei
-  try {
-    const { data } = await axios({
-      method: "get",
-      url: process.env.IS_PROD
-        ? "https://gasstation-mainnet.matic.network/v2"
-        : "https://gasstation-mumbai.matic.today/v2",
-    });
-    maxFeePerGas = ethers.utils.parseUnits(
-      Math.ceil(data.fast.maxFee) + "",
-      "gwei"
-    );
-    maxPriorityFeePerGas = ethers.utils.parseUnits(
-      Math.ceil(data.fast.maxPriorityFee) + "",
-      "gwei"
-    );
-  } catch (error) {
-    console.error("Error fetching gas prices:", error);
-    return;
-  }
-
-  try {
-    // Send the transaction
-    const tx = await erc20dint.transfer(destAddr, amount, {
-      maxFeePerGas: ethers.utils.parseUnits("350", "gwei"),
-      maxPriorityFeePerGas: ethers.utils.parseUnits("95", "gwei"),
-      gasLimit: ethers.utils.parseUnits("8000000", "wei"),
-    });
-    
-    const receipt = await tx.wait();
-    console.log("Transaction Hash", receipt.transactionHash);
-    return receipt;
-  } catch (error) {
-    console.error("Error sending transaction:", error);
-    return;
-  }
+  const receipt = await tx.wait();
+  console.log("Transaction Hash", receipt.transactionHash);
+  return receipt;
 };
 
 module.exports = { transferDint };
