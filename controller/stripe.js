77c1730f7a28d956b4ab77f368cac8442f90c0df
Max <info@dint.com>
2023-03-07 18:26:19 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -33,7 +33,7 @@ const transferDint = async ({ amount, destAddr }) => {
   try {
     const tx = await erc20dint.transfer(destAddr, amount, {
       maxFeePerGas: ethers.utils.parseUnits("450", "gwei"),
-      gasLimit: ethers.utils.parseUnits("20000000", "wei"),
+      gasLimit: ethers.utils.parseUnits("25000000", "wei"),
     });
     console.log("Transaction hash:", tx.hash);
   } catch (error) {
