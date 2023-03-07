61b22bf035016137d08baee0f5c0fb479c8e5568
Max <info@dint.com>
2023-03-07 18:39:25 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,48 +1,56 @@
 const ethers = require("ethers");
+const fetch = require("node-fetch");
+
 require("dotenv").config();
 
-const transferDint = async ({ amount, destAddr }) => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.RPC_PROVIDER
-  );
+const DINT_TOKEN_ABI = [
+  {
+    constant: false,
+    inputs: [
+      { name: "_to", type: "address" },
+      { name: "_amount", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ name: "success", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
-  const signer = new ethers.Wallet(
-    (process.env.OWNER_PRIVATE_KEY),
-    provider
-  );
-  const abi = [
-    {
-      constant: false,
-      inputs: [
-        { name: "_to", type: "address" },
-        { name: "_amount", type: "uint256" },
-      ],
-      name: "transfer",
-      outputs: [{ name: "success", type: "bool" }],
-      payable: false,
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-  ];
+const GAS_LIMIT = ethers.utils.parseUnits("25000000", "wei");
+
+const transferDint = async ({ amount, destAddr }) => {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
+
+  const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
 
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
-  const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+  const erc20dint = new ethers.Contract(contractAddr, DINT_TOKEN_ABI, signer);
 
-  // Fetch the current gas price from GasNow
-  const gasPrice = await fetch('https://gasstation-mainnet.matic.network/')
-    .then(response => response.json())
-    .then(data => data.data.standard);
+  const gasPrice = await getGasPrice();
 
   try {
     const tx = await erc20dint.transfer(destAddr, amount, {
-      gasPrice: gasPrice,
-      gasLimit: ethers.utils.parseUnits("25000000", "wei"),
+      gasPrice,
+      gasLimit: GAS_LIMIT,
     });
     console.log("Transaction hash:", tx.hash);
   } catch (error) {
     console.log("Error transferring DINT:", error);
   }
-  
+};
+
+const getGasPrice = async () => {
+  try {
+    const response = await fetch(process.env.POLYGON_GAS_API);
+    const { data } = await response.json();
+    return data.standard;
+  } catch (error) {
+    console.log("Error fetching gas price:", error);
+    return ethers.utils.parseUnits("20", "gwei"); // fallback gas price
+  }
 };
 
 module.exports = { transferDint };
+
