57d5c17b2685f169ce3562507d7d3dd43a5e8171
Max <info@dint.com>
2023-03-07 19:14:11 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -2,62 +2,66 @@ const ethers = require("ethers");
 const axios = require("axios");
 require("dotenv").config();
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
-const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
-const contractAddr = process.env.DINT_TOKEN_ADDRESS;
-const abi = [
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_amount", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
+const transferDint = async ({ amount, destAddr }) => {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
 
-const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+  const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider);
 
-const getGasPrices = async () => {
+  const abi = [
+    {
+      constant: false,
+      inputs: [
+        { name: "_to", type: "address" },
+        { name: "_amount", type: "uint256" },
+      ],
+      name: "transfer",
+      outputs: [{ name: "success", type: "bool" }],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+
+  const contractAddr = process.env.DINT_TOKEN_ADDRESS;
+  const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+
+  // get max fees from gas station
+  let maxFeePerGas = ethers.BigNumber.from(150000000000); // fallback to 40 gwei
+  let maxPriorityFeePerGas = ethers.BigNumber.from(1500000000000); // fallback to 40 gwei
   try {
-    const url = process.env.IS_PROD
-      ? "https://gasstation-mainnet.matic.network/v2"
-      : "https://gasstation-mumbai.matic.today/v2";
-    const { data } = await axios.get(url);
-    const maxFeePerGas = ethers.utils.parseUnits(
+    const { data } = await axios({
+      method: "get",
+      url: process.env.IS_PROD
+        ? "https://gasstation-mainnet.matic.network/v2"
+        : "https://gasstation-mumbai.matic.today/v2",
+    });
+    maxFeePerGas = ethers.utils.parseUnits(
       Math.ceil(data.fast.maxFee) + "",
       "gwei"
     );
-    const maxPriorityFeePerGas = ethers.utils.parseUnits(
+    maxPriorityFeePerGas = ethers.utils.parseUnits(
       Math.ceil(data.fast.maxPriorityFee) + "",
       "gwei"
     );
-    return { maxFeePerGas, maxPriorityFeePerGas };
   } catch (error) {
     console.error("Error fetching gas prices:", error);
-    throw error;
+    return;
   }
-};
-
-const transferDint = async ({ amount, destAddr }) => {
-  const { maxFeePerGas, maxPriorityFeePerGas } = await getGasPrices();
 
   try {
+    // Send the transaction
     const tx = await erc20dint.transfer(destAddr, amount, {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-      gasLimit: ethers.utils.parseUnits("8000000", "wei"),
+      maxFeePerGas: ethers.utils.parseUnits("500", "gwei"),
+      maxPriorityFeePerGas: ethers.utils.parseUnits("150", "gwei"),
+      gasLimit: ethers.utils.parseUnits("1000000", "wei"),
     });
+    
     const receipt = await tx.wait();
-    console.log("Transaction Hash:", receipt.transactionHash);
+    console.log("Transaction Hash", receipt.transactionHash);
     return receipt;
   } catch (error) {
     console.error("Error sending transaction:", error);
-    throw error;
+    return;
   }
 };
 
