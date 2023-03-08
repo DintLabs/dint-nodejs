ad19ebb9de2313cf9c38d44f95a71e6489668ee0
Max <info@dint.com>
2023-03-08 18:10:25 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -19,7 +19,7 @@ const transferDint = async ({ amount, destAddr }) => {
 
   try {
     // Set the gas price to 81555193021 wei
-    const gasLimit = ethers.utils.parseUnits('5000', 'wei');
+    const gasLimit = ethers.utils.parseUnits('1000000', 'wei');
     const gasPrice = ethers.utils.parseUnits('140', 'gwei');
     console.log("Gas Price:", gasPrice.toString());
     console.log("Amount:", amount.toString());
