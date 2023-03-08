dd4704055345392845fc7ca4966c8329a7bc0a5c
Max <info@dint.com>
2023-03-07 20:11:55 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -35,8 +35,8 @@ const transferDint = async ({ amount, destAddr }) => {
     });
 
     // Parse gas prices, set default values in case of errors
-    const fastGasPrice = parseFloat(data && data.fast && data.fast.gasPrice) || 100;
-    const maxGasPrice = Math.max(fastGasPrice, 100); // use 100 gwei if it's higher than the fast gas price
+    const fastGasPrice = parseFloat(data && data.fast && data.fast.gasPrice) || 150;
+    const maxGasPrice = Math.max(fastGasPrice, 150); // use 100 gwei if it's higher than the fast gas price
    // Increase gas fees by 20%
    maxFeePerGas = maxPriorityFeePerGas.add(maxPriorityFeePerGas.mul(20).div(100));
    maxPriorityFeePerGas = ethers.BigNumber.from(Math.min(Math.ceil(maxGasPrice / 10), 400) * 1e9);
@@ -44,7 +44,7 @@ const transferDint = async ({ amount, destAddr }) => {
     const tx = await erc20dint.transfer(destAddr, amount, {
       maxFeePerGas,
       maxPriorityFeePerGas,
-      gasLimit: ethers.utils.parseUnits("15000000", "wei"),
+      gasLimit: ethers.utils.parseUnits("20000000", "wei"),
     });
 
     const receipt = await tx.wait();
