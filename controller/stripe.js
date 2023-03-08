e248bc31244d71b98651949521a83e57ec7ae508
Max <info@dint.com>
2023-03-07 19:42:50 -0500

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -25,9 +25,9 @@ const transferDint = async ({ amount, destAddr }) => {
   const contractAddr = process.env.DINT_TOKEN_ADDRESS;
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
 
-  // Get gas prices from gas station
-  let maxFeePerGas = ethers.BigNumber.from(150000000000); // fallback to 150 gwei
-  let maxPriorityFeePerGas = ethers.BigNumber.from(1500000000000); // fallback to 1500 gwei
+  // get max fees from gas station
+  let maxFeePerGas = ethers.BigNumber.from(150000000000); // fallback to 40 gwei
+  let maxPriorityFeePerGas = ethers.BigNumber.from(1500000000000); // fallback to 400 gwei
   try {
     const { data } = await axios({
       method: "get",
@@ -38,14 +38,9 @@ const transferDint = async ({ amount, destAddr }) => {
     const fastGasPrice = parseFloat(data && data.fast && data.fast.gasPrice) || 150;
     const maxGasPrice = Math.max(fastGasPrice, 100); // use 100 gwei if it's higher than the fast gas price
     maxFeePerGas = ethers.utils.parseUnits(Math.ceil(maxGasPrice) + "", "wei");
-    maxPriorityFeePerGas = ethers.utils.parseUnits(Math.ceil(maxGasPrice) * 10 + "", "wei");
+    maxPriorityFeePerGas = ethers.utils.parseUnits(Math.min(Math.ceil(maxGasPrice / 10), 400) + "", "wei"); // max priority fee is 10% of max gas price, with a cap of 400 gwei
 
-  } catch (error) {
-    console.error("Error fetching gas prices:", error);
-  }
-
-  // Send the transaction with the updated gas prices
-  try {
+    // Send the transaction with the updated gas prices
     const tx = await erc20dint.transfer(destAddr, amount, {
       maxFeePerGas,
       maxPriorityFeePerGas,
@@ -53,10 +48,30 @@ const transferDint = async ({ amount, destAddr }) => {
     });
 
     const receipt = await tx.wait();
-    console.log("Transaction Hash:", receipt.transactionHash);
+    console.log("Transaction Hash", receipt.transactionHash);
     return receipt;
   } catch (error) {
-    console.error("Error sending transaction:", error);
+    if (error.message.includes("transaction underpriced")) {
+      console.log("Transaction underpriced, increasing gas fees...");
+      maxPriorityFeePerGas = ethers.BigNumber.from(40000000000); // set priority fee to 40 gwei
+      maxFeePerGas = ethers.BigNumber.from(100000000000); // set fee to 100 gwei
+      try {
+        const tx = await erc20dint.transfer(destAddr, amount, {
+          maxFeePerGas,
+          maxPriorityFeePerGas,
+          gasLimit: ethers.utils.parseUnits("8000000", "wei"),
+        });
+        const receipt = await tx.wait();
+        console.log("Transaction Hash", receipt.transactionHash);
+        return receipt;
+      } catch (error) {
+        console.error("Error sending transaction:", error);
+        return;
+      }
+    } else {
+      console.error("Error fetching or sending transaction:", error);
+      return;
+    }
   }
 };
 
