7f0e8dadc5ff89d2145560781b73d8f953c6b703
Max <info@dint.com>
2023-03-09 17:25:28 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -226,7 +226,7 @@ const send = async (data, value) => {
     dintDistContract
       .sendDint(data.userAddress, data.recieverAddress, value, {
         gasLimit: gasLimit, 
-        gasPrice: gasPrice,
+        gasPrice: ethers.BigNumber.from(gasPrice),
       })
 
       .then(
