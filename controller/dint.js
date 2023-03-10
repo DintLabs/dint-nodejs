b9a58f27615a155c0eec01f4a62e595bcce9388a
Max <info@dint.com>
2023-03-10 11:49:49 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -33,7 +33,7 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
 const ownerSigner = new ethers.Wallet(ownerPrivateKey, provider);
 
 const generate = async (data, amount) => {
-  const nonce = 0;
+
   if (amount >= 0) {
     const signer = new ethers.Wallet(data.userPrivateKey, provider);
     const contract = new ethers.Contract(
