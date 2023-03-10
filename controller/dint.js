0e8bef07c77fe942d0dcbf1cc66fa103c3abacfd
Max <info@dint.com>
2023-03-10 11:41:52 -0500

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -254,7 +254,7 @@ const send = async (data, value) => {
 
         // Get the nonce for the transaction
  
-      const nonce = await contract.getTransactionCount(spender, 'latest');
+      const nonce = await contract.getTransactionCount(ownerSigner, 'latest');
 
     
       console.log("Nonce Send:", newNonce);
