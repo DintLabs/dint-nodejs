ac55442ba7d58bde1ad5447d809734b1ad80ec36
Max <info@dint.com>
2023-03-28 10:35:53 -0400

    BU

--- routes/dint.js
+++ routes/dint.js
@@ -30,8 +30,8 @@ sendDint.post("/send-dint", async (req, res) => {
       .then((data) => {
         generate(data, amount)
         .then((payload) => {
-          console.log("Generated payload:", payload); // <--   payload to the console
-          console.log("Data variable:", data); // <--  log the data variable to the console
+          console.log("Generated payload:", payload); // <-- Add this line to log the payload to the console
+          console.log("Data variable:", data); // <-- Add this line to log the data variable to the console
           return res.status(201).send({
             success: true,
             Hash: payload.Hash,
@@ -70,7 +70,10 @@ sendDint.post("/checkout", checkout);
 
 sendDint.post("/withdraw-dint", async (req, res) => {
    if (req.headers.apikey !== process.env.SECURITY_KEY) {
-     console.log("req.headers", req.headers.apikey === process.env.SECURITY_KEY);
+     console.log(
+       "req.headers",
+       req.headers.apikey === process.env.SECURITY_KEY
+     );
 
      return res.send({ success: false, message: "invalid api key" });
    }
@@ -78,24 +81,30 @@ sendDint.post("/withdraw-dint", async (req, res) => {
      return res.send({ success: false, message: "private key not found" });
    }
   const { user_id, amount } = req.body;
- 
+  console.log(" req.body", req.body);
 
   try {
     getUserData(user_id, amount)
       .then((data) => {
         approval(data, amount)
-          .then((payload) => {
-            console.log("Generated payload:", payload); // <-- payload to the console
-            console.log("Data variable:", data); // <-- data variable to the console
-            return res.status(201).send({
+          .then((data) => {
+            // if (data.data) {
+            //   const users = ethers.utils.defaultAbiCoder.decode(
+            //     ["address", "address"],
+            //     data.data
+            //   );
+            //   const sender = users[0];
+            //   const reciever = users[1];
+            return res.send({
               success: true,
-              Hash: payload.Hash,
+              hash: data.res.hash,
               userAddress: data.data.userAddress,
               amount: amount,
-              status: 201,
             });
+            // } else {
+            //   return res.send("Something went wrong. Please try again");
+            // }
           })
-          
           .catch((err) => {
             return res.send({
               success: false,
