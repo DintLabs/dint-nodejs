633126ef27d9090f95865e69e57d91a2a18e8acf
Max <info@dint.com>
2023-03-28 17:35:58 -0400

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -110,7 +110,7 @@ const generate = async (data, amount) => {
         } catch (error) {
           console.log("gas error");
           console.error(error);
-          return ethers.utils.parseUnits("250", "gwei");
+          return ethers.utils.parseUnits("200", "gwei");
         }
       };
  // Get the current gas price
@@ -122,7 +122,7 @@ const generate = async (data, amount) => {
  console.log("Nonce:", nonce);
 
  // Set the gas limit to 70,000 units
- const gasLimit = ethers.utils.parseUnits('1000000', 'wei');
+ const gasLimit = ethers.utils.parseUnits('70000', 'wei');
       
       return new Promise(async (resolve, reject) => {
         contract
