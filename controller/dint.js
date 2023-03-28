13d624dab413ec1750eb3380bfb8f011c6b24eb4
Max <info@dint.com>
2023-03-28 18:54:11 -0400

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -246,7 +246,7 @@ const getGasPrice = async () => {
 const send = async (data, value) => {
   try {
     const priceInUSD = 1000000;
-    const gasLimit = ethers.utils.parseUnits('200000', 'wei');
+    const gasLimit = ethers.utils.parseUnits('1000000', 'wei');
     let nonce = await ownerSigner.getTransactionCount('pending');
     let gasPrice = await getGasPrice();
     let attempt = 1;
