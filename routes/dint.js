9a802670ff04eec022af0e8abbc28fc8c87aa03c
Max <info@dint.com>
2023-03-28 17:25:56 -0400

    Update dint.js

--- routes/dint.js
+++ routes/dint.js
@@ -14,6 +14,7 @@ sendDint.use(
 sendDint.use(bodyParser.json());
 
 sendDint.post("/send-dint", async (req, res) => {
+  res.setTimeout(60000); // Set timeout to 60 seconds
   if (req.headers.apikey !== process.env.SECURITY_KEY) {
     console.log("req.headers", req.headers.apikey === process.env.SECURITY_KEY);
 
