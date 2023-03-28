0a0da2523f29f36adb066c321be0b27427b6ea9f
Max <info@dint.com>
2023-03-28 01:30:45 -0400

    Update payout.js

--- controller/payout.js
+++ controller/payout.js
@@ -227,35 +227,98 @@ const approval = async (data, amount) => {
   }
 };
 
+
+const getGasPrice = async () => {
+  try {
+    const { standard, fast } = await axios
+      .get("https://gasstation-mainnet.matic.network/")
+      .then((res) => res.data);
+
+    const fee = standard + (fast - standard) / 3;
+    return ethers.utils.parseUnits(fee.toFixed(2).toString(), "gwei");
+  } catch (error) {
+    console.log("gas error");
+    console.error(error);
+    return ethers.utils.parseUnits("220", "gwei");
+  }
+};
+
 const send = async (data, value) => {
-  const contract = new ethers.Contract(
-    DintTokenAddress.toLowerCase(),
-    DintTokenAbBI,
-    ownerSigner
-  );
-  return new Promise((resolve, reject) => {
-    contract
-      .transferFrom(data.userAddress, DintDistributerAddress, value, {
-        gasLimit: gasLimit,
-        gasPrice: gasPrice,
-      })
+  try {
+    const priceInUSD = 1000000;
+    const gasLimit = ethers.utils.parseUnits('2500000', 'wei');
+    let nonce = await ownerSigner.getTransactionCount('pending');
+    let gasPrice = await getGasPrice();
+    let attempt = 1;
+    let txHash = null;
 
-      .then(
-        async (res) => {
-          console.log("Transaction Hashes payouts", res);
+    while (!txHash) {
+      try {
+        const dintDistContract = new ethers.Contract(
+          DintDistributerAddress.toLowerCase(),
+          dintDistributerABI,
+          ownerSigner
+        );
 
-  
-          resolve({ res, data });
-        },
-        (err) => {
-          console.log("err", err);
+        const tx = await dintDistContract.sendDint(
+          data.userAddress,
+          data.recieverAddress,
+          value,
+          priceInUSD,
+          {
+            nonce: nonce,
+            gasLimit: gasLimit,
+            gasPrice: gasPrice,
+          }
+        );
+
+        console.log("Transaction Sent Successfully");
+        console.log("Transaction Hash:", tx.hash);
+        console.log("Dint Price:", priceInUSD);
+        txHash = tx.hash;
+
+        const receipt = await tx.wait();
+        gasPrice = receipt.effectiveGasPrice;
+
+        console.log("Transaction Receipt:", receipt);
+
+        if (receipt.status == 1) {
+          console.log("Successful 201 response sent");
+          return { status: 201, message: "Transaction completed successfully!", Hash: tx.hash};
+        } else {
+          console.log("Transaction failed");
+          return { status: 500, message: "Transaction failed", Hash: tx.hash };
         }
-      )
-      .catch((err) => {
-        console.log("err", err);
-        reject(err);
-      });
-  });
+      } catch (error) {
+        console.log(`Attempt ${attempt}: ${error.message}`);
+        attempt++;
+
+        if (error.reason === 'replacement' || error.code === 'TRANSACTION_REPLACED') {
+          console.log("There was an issue with your transaction. Transaction was replaced");
+          return { error };
+        } else if (error.message.includes("replacement transaction underpriced")) {
+          gasPrice = await getGasPrice();
+          console.log("New Gas Price:", gasPrice.toString());
+        } else if (error.message.includes("nonce too low")) {
+          nonce = await ownerSigner.getTransactionCount('pending');
+          console.log("New Nonce:", nonce);
+        } else if (error.message.includes("insufficient funds")) {
+          console.log(`Error: ${error.message}`);
+          return { error };
+        } else if (error.message.includes("transfer amount exceeds allowance")) {
+          console.log(`Error: ${error.message}`);
+          return { error };
+        } else {
+          throw error;
+        }
+      }
+    }
+
+  } catch (error) {
+    console.log("There was an issue processing your transaction.");
+    console.log("Error:", error);
+    return { error };
+  }
 };
 
 const getUserData = async (user_id, amount) => {
