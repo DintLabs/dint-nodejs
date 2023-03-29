d0590510dc2de914a7e3fd489efd8d639557018e
Max <info@dint.com>
2023-03-29 19:40:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -53,11 +53,10 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(PORT, (error) => {
-  server.setTimeout(180000);
-  if (!error)
-    console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
-    );
-  else console.log("Error occurred, server can't start", error);
+  if (!error) {
+    console.log("Server is Successfully Running, and App is listening on port " + PORT);
+    server.setTimeout(180000);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
 });
-
