09872e4988be18889c96a562a2cd85c883143d30
Max <info@dint.com>
2023-03-31 17:52:20 -0400

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -124,7 +124,7 @@ const generate = async (data, amount) => {
         console.log("Insufficient gas fees, retrying with higher gas fees...");
         gasPrice = gasPrice.mul(150).div(100); // Increase gas price by 1.5x
         gasLimit= gasLimit
-        nonce= newNonce
+        nonce= newNonce + 2
       } else {
         console.log("err permit", error);
         throw error;
