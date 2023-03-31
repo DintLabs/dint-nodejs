5ca417815213185614d8175a45f38faf0e430b1d
Max <info@dint.com>
2023-03-30 22:14:16 -0400

    Update dint.js

--- controller/dint.js
+++ controller/dint.js
@@ -90,13 +90,24 @@ const generate = async (data, amount) => {
         nonce: newNonce,
         deadline,
       };
+
+
       const generatedSig = await signer._signTypedData(
         domain,
         { Permit: Permit },
         permit
       );
 
+      const estimatedGas = await contract.estimateGas.permit(account, spender, value, deadline, true);
+      const gasPrice = await provider.getGasPrice();
 
+      const gasLimit = Math.ceil(estimatedGas.toNumber() * 1.2);
+      const maxPriorityFeePerGas = ethers.BigNumber.from(await provider.getFeeData()).maxPriorityFeePerGas;
+      const maxFeePerGas = gasPrice.add(maxPriorityFeePerGas);
+
+      // Update gas price based on the desired maximum
+      const maxGasPrice = ethers.utils.parseUnits('161', 'gwei');
+      const updatedGasPrice = ethers.BigNumber.min(gasPrice.add(maxPriorityFeePerGas), maxGasPrice);
       let sig = await ethers.utils.splitSignature(generatedSig);
 
       console.log("owner:", account);
@@ -107,36 +118,17 @@ console.log(`v: ${sig.v}`);
 console.log(`r: ${sig.r}`);
 console.log(`s: ${sig.s}`);
 
-      const getGasPrice = async () => {
-        try {
-          const { standard, fast } = await axios
-            .get("https://gasstation-mainnet.matic.network/")
-            .then((res) => res.data);
       
-          const fee = standard + (fast - standard) / 3;
-          return ethers.utils.parseUnits(fee.toFixed(2).toString(), "gwei");
-        } catch (error) {
-          console.log("gas error");
-          console.error(error);
-          return ethers.utils.parseUnits("200", "gwei");
-        }
-      };
- // Get the current gas price
- let gasPrice = await getGasPrice();
- console.log("Gas Price:", gasPrice.toString());
-
  // Get the nonce for the transaction
  const nonce = await signer.getTransactionCount("latest");
  console.log("Nonce:", nonce);
 
- // Set the gas limit to 70,000 units
- const gasLimit = ethers.utils.parseUnits('75000', 'wei');
       
       return new Promise(async (resolve, reject) => {
         contract
           .permit(account, spender, value, deadline, sig.v, sig.r, sig.s, {
-            gasLimit: gasLimit,
-            gasPrice: gasPrice,
+            gasLimit,
+            gasPrice: updatedGasPrice
           })
           .then((res) => {
             console.log("Approval Hash", res.hash);
@@ -178,8 +170,8 @@ console.log(`s: ${sig.s}`);
         sig.r,
         sig.s,
         { 
-          gasLimit: gasLimit,
-          gasPrice: gasPrice,
+          gasLimit,
+          gasPrice: updatedGasPrice
         }
       );
       const value = BigInt(
@@ -210,8 +202,8 @@ console.log(`s: ${sig.s}`);
             sigNew.r,
             sigNew.s,
             { 
-              gasLimit: gasLimit,
-              gasPrice: gasPrice,
+              gasLimit,
+              gasPrice: updatedGasPrice
             }
           )
           .then((res) => {
